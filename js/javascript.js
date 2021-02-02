0d070239a55fc5d8c51aef9ced59928082630835
ahmed <ahmed3bdo008@gmail.com>
2021-02-02 18:37:07 +0200

    edit json file

--- js/javascript.js
+++ js/javascript.js
@@ -1,35 +1,16 @@
+var data;
 window.onload = function () {
-    display();
+   data= openJsonFile();
 };
 
 function display() {
 
+  openJsonFile()
 }
+
 async function openJsonFile() {
-    async function getSelectElement() {
-        result = await fetch("../josn/json.json");
-        result = await result.json()
-        alert(result)
-        for (const iterator of result) {
-            console.log(iterator)
-            selectlist.innerHTML += `<option>${iterator.name}</option>`
-        }
-
-        var btn1 = document.createElement('button')
-
-        btn1.addEventListener('click', btnclick)
-        btn1.append("show")
-        document.getElementById('selectdiv').appendChild(btn1);
-
-
-
-    }
-}
-openJsonFile()
-async function getSelectElement() {
-    result = await fetch("E:\home\js\json.json");
+    result = await fetch("./data/json.json");
     result = await result.json()
-    alert(result)
     for (const iterator of result) {
         console.log(iterator)
         selectlist.innerHTML += `<option>${iterator.name}</option>`
@@ -37,12 +18,6 @@ async function getSelectElement() {
 
     var btn1 = document.createElement('button')
 
-    btn1.addEventListener('click', btnclick)
-    btn1.append("show")
-    document.getElementById('selectdiv').appendChild(btn1);
-
-
 
 }
 /////////CALL Method:fetch
-getSelectElement()
